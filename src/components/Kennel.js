d40f9d752e98305f15e32e94e51d522d29d26f25
Alex <o.a.momotyuk@gmail.com>
2019-09-29 22:27:41 -0500

    Chapter 2 - Practice-Kennel Routes

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,32 +1,18 @@
-import React, { Component } from 'react'
-import AnimalCard from './animal/AnimalCard'
-import EmployeeCard from './employee/EmployeeCard'
-import LocationCard from './location/LocationCard'
-import OwnerCard from './owner/OwnerCard'
-import './Kennel.css'
+import React, { Component } from "react"
+import NavBar from "./nav/NavBar"
+import ApplicationViews from "./ApplicationViews"
+
+import "./Kennel.css"
 
 class Kennel extends Component {
     render() {
         return (
-            <div>
-                <div>
-                    <h2>Student Kennels<br />
-                        <small>Loving care when you're not there.</small>
-                    </h2>
-                    <address>
-                        Visit Us at the Nashville North Location
-            <br />500 Puppy Way
-          </address>
-                </div>
-                <div>
-                    <AnimalCard />
-                    <EmployeeCard />
-                    <LocationCard />
-                    <OwnerCard />
-                </div>
-            </div>
-        );
+            <>
+                <NavBar />
+                <ApplicationViews />
+            </>
+        )
     }
 }
 
-export default Kennel;
+export default Kennel
