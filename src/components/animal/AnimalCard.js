175c4cd3e4c276fa6f94c99bdd0d1c7caff2b78b
Alex <o.a.momotyuk@gmail.com>
2019-10-02 23:39:44 -0500

    pics file names are taking from database now

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { firstLetterCase } from '../../modules/helpers'
+import "./AnimalCard.css"
 
 class AnimalCard extends Component {
 
@@ -9,17 +10,19 @@ class AnimalCard extends Component {
   }
 
   render() {
+
     return (
       <div className="card">
         <div className="card-content">
           <picture>
-            <img src={require('./dog.svg')} alt="My Dog" />
+            <img src={require(`./images/${this.props.animal.imageUrl}`)} alt="dogs pics" />
           </picture>
           <h3>Name: <span className="card-petname">{firstLetterCase(this.props.animal.name)}</span></h3>
           <p>Breed: {this.props.animal.breed}</p>
           <Link to={`/animals/${this.props.animal.id}`}><button>Details</button></Link>
           {/*<button type="button" onClick={() => this.props.deleteAnimal(this.props.animal.id)}>Discharge</button>*/}
           <button type="button" onClick={() => this.myDelete()}>Discharge</button>
+          <button type="button" onClick={() => { this.props.history.push(`/animals/${this.props.animal.id}/edit`) }}>Edit</button>
         </div>
       </div>
     );
